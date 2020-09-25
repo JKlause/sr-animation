8f5264ed7e6c5beeabcf256b98c4e248cf64fb20
Joe Klause <joeklause@gmail.com>
2020-09-25 15:29:30 -0700

    about me content

--- src/components/AboutDrawer.js
+++ src/components/AboutDrawer.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import joeImg from '../assets/joeImg.jpg';
 import styles from './AboutDrawer.scss';
 
 export default function AboutDrawer({ isOpen }) {
 
   return (
-    <section className={`${styles.AboutDrawer} ${isOpen ? 'open' : ''}`}>
-      im the about drawer
+    <section className={`${styles.AboutDrawer} ${isOpen ? 'open' : ''} disable-scrollbars`}>
+      <h1>
+        { 'Hi I\'m Joe'}
+      </h1>
+      <img src={joeImg} alt="Joe's Picture" />
     </section>
   );
 }
