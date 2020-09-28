ddf718903ab460d9b7fee2908a0d9c31cce1cdf7
Joe Klause <joeklause@gmail.com>
2020-09-28 11:17:44 -0700

    small change to content

--- src/components/about-drawer/AboutDrawerDropdown.js
+++ src/components/about-drawer/AboutDrawerDropdown.js
@@ -10,7 +10,7 @@ export default function AboutDrawerDropdown({ isOpen }) {
     <section className={`${styles.AboutDrawerDropdown} ${isOpen ? 'open' : 'closed'}`}>
       <FillParent tag="section" className="content" >
         <p>
-          {'This site was written using React.js, Sass, the ZX library, and a plethora of animations and transitions.'}
+          {'This site was written using React.js, Sass, the @vx library, and a plethora of animations and transitions.'}
         </p>
         <p>
           {'It is deployed on Heroku, served up with a simple express server, built in VSCode utilizing Github, and was written over the weekend of 9/25/2020.'}
