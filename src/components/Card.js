a2b57e43d2fc27c93074d55c18603b4de481fc02
Joe Klause <joeklause@gmail.com>
2020-09-28 16:59:08 -0700

    added refresh button

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,9 +6,12 @@ const multiplierSet = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
 export default function Card({ Visualization, Info, data }) {
   const [renderedData, setRenderedData] = useState(data);
+  const [isResetDisabled, setIsResetDisabled] = useState(true);
   const containerRef = useRef();
 
   const handleUpdateDataClick = () => {
+    if(isResetDisabled) setIsResetDisabled(false);
+
     setRenderedData(renderedData.map(({ label, value }) => {
       const multiplier = multiplierSet[Math.floor(Math.random() * multiplierSet.length)];
       return {
@@ -18,17 +21,27 @@ export default function Card({ Visualization, Info, data }) {
     }));
   };
 
+  const handleResetDataClick = () => {
+    setIsResetDisabled(true);
+    setRenderedData(data);
+  };
+
 
   return (
     <section className={styles.Card}>
       <section className="content">
 
         <section className="graph-container disable-scrollbars" ref={containerRef}>
-          <Visualization data={renderedData} containerRef={containerRef}/>
+          <Visualization 
+            data={renderedData} 
+            containerRef={containerRef}/>
         </section>
 
         <section className="info-container disable-scrollbars">
-          <Info handleUpdateDataClick={handleUpdateDataClick} />
+          <Info 
+            handleUpdateDataClick={handleUpdateDataClick} 
+            handleResetDataClick={handleResetDataClick}
+            isResetDisabled={isResetDisabled}/>
         </section>
 
       </section>
