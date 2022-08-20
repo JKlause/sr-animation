10e0373f5d53b9c71ced9ae07a4507130e42add7
Joe Klause <joeklause@gmail.com>
2022-08-20 15:50:10 -0700

    create text components

---
+++ src/components/about-drawer/AboutDrawerTextComponents.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export const AboutDrawerParagraph = ({text}) => (
+  <p>{text}</p>
+);
+
+export const AboutDrawerParagraphHeading = ({headerText}) => (
+  <h2>{headerText}</h2>
+);
