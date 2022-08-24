d1717a6c61ecc7529c3a6203da2ed26e99375058
Joe Klause <joeklause@gmail.com>
2022-08-23 17:40:02 -0700

    changes

--- src/components/about-drawer/AboutDrawerTextComponents.js
+++ src/components/about-drawer/AboutDrawerTextComponents.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-
-export const AboutDrawerParagraph = ({text}) => (
+ 
+export const AboutDrawerParagraph = ({ text }) => (
   <p>{text}</p>
 );
 
-export const AboutDrawerParagraphHeading = ({headerText}) => (
+export const AboutDrawerParagraphHeading = ({ headerText }) => (
   <h2>{headerText}</h2>
 );
