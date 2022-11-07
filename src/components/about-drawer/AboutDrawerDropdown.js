68232a3595c40464b31ec5f7a70aec09214b6af1
Joe Klause <joeklause@gmail.com>
2022-11-06 17:45:13 -0800

    updated about drawer info

--- src/components/about-drawer/AboutDrawerDropdown.js
+++ src/components/about-drawer/AboutDrawerDropdown.js
@@ -18,9 +18,8 @@ export default function AboutDrawerDropdown({ isOpen, dropdownRef }) {
           rel="noopener noreferrer">
           {'source code'}
         </a>
-        {' is available on GitHub, and was written primarily over the weekend of 9/25/2020.'}
+        {' is available on GitHub.'}
       </p>
-      <AboutDrawerParagraph text={'It has been more recently updated over the weekend of 8/20/2022.'} />
       <p>
         {'I owe a fair amount of gratitude to Joel M. Turner\'s blog posts on @vx '}
         <a
